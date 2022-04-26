e144d1a62aa4ab6254773d4ffa974873ac4eb825
Herbert Araújo <herbertaraujo.contact@gmail.com>
2022-04-26 14:52:04 -0300

    finished: auth

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { SingInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="logo" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+
+        <SingInButton />
+      </div>
+    </header>
+  );
+}

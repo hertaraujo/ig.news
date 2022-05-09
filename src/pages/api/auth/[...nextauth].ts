c14cab2fc49e7b266279385ed55096a3d6022789
Herbert Araújo <herbertaraujo.contact@gmail.com>
2022-05-09 16:04:48 -0300

    finished: post preview

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,6 +20,35 @@ export default NextAuth({
   ],
   // signingKey: process.env.SIGNING_KEY,
   callbacks: {
+    async session({ session, token, user }) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+
+        console.log(`session try`);
+        return { ...session, activeSubscription: userActiveSubscription };
+      } catch {
+        console.log(`session catch`);
+        return { ...session, activeSubscription: null };
+      }
+    },
     async signIn({ user, account, profile, email, credentials }) {
       try {
         await fauna.query(
---
+++ src/pages/posts/preview/[slug].tsx
@@ -0,0 +1,91 @@
+import { GetStaticProps } from "next";
+import Link from "next/link";
+import { RichText } from "prismic-reactjs";
+import * as prismicH from "@prismicio/helpers";
+import { getPrismicClient } from "../../../services/prismic";
+import Head from "next/head";
+
+import styles from "../post.module.scss";
+import { getSession, useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+interface PostPreviewProps {
+  post: {
+    slug: string;
+    title: string;
+    excerpt: string;
+    content: string;
+    updatedAt: string;
+  };
+}
+
+export default function PostPreview({ post }: PostPreviewProps) {
+  const session = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session?.data.activeSubscription) {
+      router.push(`/posts/${post.slug}`);
+    }
+  }, [session]);
+
+  return (
+    <>
+      <Head>
+        <title>{post.title}</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div
+            className={`${styles.postContent} ${styles.previewContent}`}
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+          <div className={styles.continueReading}>
+            Wanna continue reading?
+            <Link href="/">
+              <a>Subscribe now 🤗</a>
+            </Link>
+          </div>
+        </article>
+      </main>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient();
+
+  const response = await prismic.getByID(String(slug));
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: prismicH.asHTML(response.data.content.splice(0, 5)),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  };
+
+  return {
+    props: { post },
+    revalidate: 60 * 30, //30 min
+  };
+};
